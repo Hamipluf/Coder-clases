f9b4b3a119403856a184dae4368e73a4a0e6068d
Ramiro Gumma <ramirogumma@hotmail.com>
2023-01-18 10:46:04 -0300

    Finish Desafio clase 6

--- clase6/src/app.js
+++ clase6/src/app.js
@@ -6,31 +6,32 @@ var manager = new ProductManager("Products.json");
 
 // devuelve el producto por limit por las querys
 app.get("/products", async (req, res) => {
+  // este endpoint devuelve un arreglo [productos]
+  const { limit } = req.query; // devuelve string
   try {
     const productSave = await manager.getProducts();
-    if (productSave === null) {
-      res.status(404).send("No hay productos");
-    } else {
-      const { limit } = req.query;
-      const productosLimit = productSave.slice(0, limit);
-      res.status(200).send(productosLimit);
-    }
+
+    const productosLimit = productSave.slice(0, parseInt(limit));
+
+    res.status(200).send(productosLimit);
   } catch (error) {
     console.log(error);
+    res.status(500).send("message:", error.message, "Reason:", error.name);
   }
 });
-
+// devuelve el producto por id
 app.get("/products/:pid", async (req, res) => {
+  // devuelve un arreglo de productos []
+  const { pid } = req.params; // params son strings'
   try {
-    const { pid } = req.params;
     const getProductsById = await manager.getProductsById(parseInt(pid));
     if (getProductsById === null) {
       res.status(404).send("Producto no encontrado");
-    } else {
-      res.status(200).send(getProductsById);
     }
+    res.status(200).send(getProductsById); // devuelve {}
   } catch (error) {
     console.log(error);
+    res.status(500).send("message:", error.message, "Reason:", error.name);
   }
 });
 app.listen(3001, () => {
